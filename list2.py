6c0d923400bc73ad52bbfe5fd5ab0faf16c0a486
mpaydar <m21newyork@gmail.com>
2022-08-03 11:10:31 -0400

    remove duplicate from the list

---
+++ list2.py
@@ -0,0 +1,12 @@
+# removing duplicate from the list 
+
+list1=[1,2,1,4,5,6]
+def duplicate_remove(l):
+    list2=[]
+    for i in l:
+        if i not in list2:
+            list2.append(i)
+    return list2
+result=duplicate_remove(list1)
+print(result)
+              
